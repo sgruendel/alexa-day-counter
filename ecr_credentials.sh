d0eef54883a8b7002f9ec1d21f66ca90a91c9c76
Stefan Gründel <stefan.gruendel@gmail.com>
2019-12-31 15:29:50 +0100

    Use encrypted credentials before Travis install.

---
+++ ecr_credentials.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# see https://stackoverflow.com/a/44850245
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
