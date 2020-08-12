7ce712b4614d5d08c48c7b17e2f8528eae04dbac
Stefan Gründel <stefan.gruendel@gmail.com>
2020-08-12 09:50:25 +0200

    Remove dashbot.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 const Alexa = require('ask-sdk-core');
 const i18next = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
-const dashbot = process.env.DASHBOT_API_KEY ? require('dashbot')(process.env.DASHBOT_API_KEY).alexa : undefined;
 const winston = require('winston');
 const moment = require('moment-timezone');
 
@@ -407,4 +406,3 @@ exports.handler = Alexa.SkillBuilders.custom()
     .withApiClient(new Alexa.DefaultApiClient())
     .withSkillId(SKILL_ID)
     .lambda();
-if (dashbot) exports.handler = dashbot.handler(exports.handler);
